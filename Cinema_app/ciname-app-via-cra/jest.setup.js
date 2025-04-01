de4b2203da1789260d399fbc843dd51d3da9426b
Vladyslav Kravchenko <Vladyslav.Kravchenko@columbia.com>
2025-04-01 11:45:21 +0300

    Task 2: Unite tests with JEST

---
+++ Cinema_app/ciname-app-via-cra/jest.setup.js
@@ -0,0 +1,6 @@
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    Link: ({ children }) => <>{children}</>,
+    useNavigate: jest.fn(),
+    useLocation: jest.fn(),
+  }));
